c4a7177879ac62c8df116e85163e208fbe92aea8
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2024-08-22 19:26:22 +0500

    Time: 82 ms (15.92%), Space: 17.8 MB (38.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -7,36 +7,20 @@ class Solution:
         right = size - 1
         while left <= right:
             mid = (left + right) // 2
+            print(mid)
             if nums[mid] == target:
                 start = mid
-                # search in left part 
-                right = mid - 1
-                left = 0
-                # goingBack = mid
-                # goingForward = mid
-                
-                # while goingBack - 1 >=0 and nums[goingBack-1] == target:
-                #     start = goingBack - 1
-                #     goingBack -= 1
-                # while  goingForward+1<len(nums) and nums[goingForward+1] == target:
-                #     end = goingForward + 1
-                #     goingForward += 1
-                # break
-            elif nums[mid] < target: 
-                left = mid + 1 # right sub array     
-            else:
-                right = mid - 1 # left sub array
-        left = 0 
-        right = size - 1
-
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] == target:
                 end = mid
-                # search in right part 
-                right = size - 1
-                left = mid + 1
-              
+                goingBack = mid
+                goingForward = mid
+                
+                while goingBack - 1 >=0 and nums[goingBack-1] == target:
+                    start = goingBack - 1
+                    goingBack -= 1
+                while  goingForward+1<len(nums) and nums[goingForward+1] == target:
+                    end = goingForward + 1
+                    goingForward += 1
+                break
             elif nums[mid] < target: 
                 left = mid + 1 # right sub array     
             else:
