4c5ac343ffb46536357deff7df6e1ccfd052f2a7
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-03-13 14:35:44 +0500

    Time: 130 ms (31.07%) | Memory: 22 MB (100.00%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -16,3 +16,19 @@ class Solution:
             return res
         return myBad(nums,k) - myBad(nums,k-1)
         
+    # 1 1 0 1 1, k = 3
+    # 1, res =1
+    # 1 1, res = 1 + 2 = 3
+    # 1 1 0, res = 3 + 3 = 6
+    # 1 1 0 1, res = 6 + 4 = 10
+    # 1 1 0 1 1, res = 10 + 4 = 14
+
+    # 1 1 0 1 1, k = 2
+    # 1, res =1
+    # 1 1, res = 1 + 2 = 3
+    # 1 1 0, res = 3 + 3 = 6
+    # 1 1 0 1, res = 6 + 3 = 9
+    # 1 1 0 1 1, res = 9 + 3 = 12
+
+    # 14 - 12 == 2
+
