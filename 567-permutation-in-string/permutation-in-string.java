984666186808f982bdc16e8614564cb1a12e9fd8
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-03-21 12:24:46 +0500

    Time: 46 ms (20.10%) | Memory: 44.8 MB (25.41%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()){
+                return false;
+        }
+         
+        Map<Character,Integer> s1Freq = new HashMap<>();
+        Map<Character,Integer> s2Freq = new HashMap<>();
+
+        for (int i=0; i  <  s1.length(); i++){
+
+            s1Freq.put(s1.charAt(i), s1Freq.getOrDefault( s1.charAt(i) ,0)  +  1);
+            s2Freq.put(s2.charAt(i), s2Freq.getOrDefault( s2.charAt(i), 0)  +  1);
+        }
+
+        // System.out.println(s2Freq);
+
+        if (s1Freq.equals(s2Freq)){
+            return true;
+        }
+
+        int left = 0;
+        
+        // done till s1.length
+        for (int right= s1.length(); right<s2.length(); right++){
+            // Increase window
+            char rightChar = s2.charAt(right);
+
+            s2Freq.put(rightChar, s2Freq.getOrDefault(rightChar,0)  + 1  );
+
+
+            // Shrink Window
+            char leftChar = s2.charAt(left);
+
+            s2Freq.put(leftChar, s2Freq.get(leftChar)  -  1);
+
+            if (  s2Freq.get(leftChar)==0  ){
+                s2Freq.remove(leftChar);
+            }
+            left++; // keep window according to s1 size
+
+
+            // check 
+            if (s1Freq.equals(s2Freq)){
+                return true;
+            }
+          
+        }
+
+        return false;
+        
+    }
+}
