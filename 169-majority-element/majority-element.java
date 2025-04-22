db5ba038232e9ad59f220db7b6edea49a3751159
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-04-22 13:02:30 +0500

    Time: 13 ms (29.16%) | Memory: 49.5 MB (86.33%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,13 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // sort the array
-        // 3,2,3
-        // 2,3,3, index is 1, 3/2 is 1
+        // using hashmap
+        int n = nums.length;
+        Map<Integer, Integer> counter = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            counter.put(nums[i], counter.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        n = n / 2;
 
-        // 2,2,1,1,1,2,2
-        // 1,1,1,2,2,2,2, index = 3, 7/2 is 3
-        Arrays.sort(nums);
-        return nums[ nums.length / 2];
+        for(Map.Entry<Integer, Integer> f : counter.entrySet()){
+            if (f.getValue()>n)
+            return f.getKey();
+        }
+
+        return 0;
         
     }
 }
