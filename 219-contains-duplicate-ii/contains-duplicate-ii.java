7564ff677251686fea041bcde9322329fac5a4a8
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-04-28 18:49:22 +0500

    Time: 23 ms (27.84%) | Memory: 59.8 MB (24.97%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> data = new HashMap<Integer,Integer>();
+
+        for(int i=0; i<nums.length; i++){
+            int getIndex = data.getOrDefault(nums[i],-1);
+
+            if(getIndex!=-1){
+                int difference = Math.abs(getIndex -i);
+                if (difference<=k){
+                    return true;
+                }
+            }
+
+            data.put(nums[i], i);
+
+        }
+        return false;
+        
+    }
+}
+// 1,0,1
