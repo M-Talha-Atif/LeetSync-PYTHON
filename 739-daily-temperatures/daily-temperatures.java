d08c5b1465a5c5f9c3a98986c002d8035f263470
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-05-05 23:26:52 +0500

    Time: 100 ms (5.57%) | Memory: 63.6 MB (5.15%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,49 @@
+class Solution {
+    class Pair {
+        Integer key;
+        Integer value;
+
+        Pair(Integer key, Integer value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    // Don't use numbers as a key
+    // Use index as a key
+
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        Stack<Pair> stack = new Stack<Pair>();
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int n  = temperatures.length;
+
+        for(int i =0;  i <  n; i +=1) {
+            int temp = temperatures[i];
+
+            while (!stack.isEmpty() && stack.peek().key < temp ){
+                Pair popped = stack.pop();
+                int poppedKey = popped.key;
+                int poppedIndex = popped.value;
+                int distance = i - poppedIndex;
+                map.put( poppedIndex, distance);
+            }
+
+            stack.push( new Pair(temp,i));
+        }
+
+        for(int i=0; i<n ; i+=1){
+            temperatures[i] = map.getOrDefault(i , 0);
+        }
+
+        return temperatures;
+        
+    }
+}
+
+// 73, 74
+
+// 73, 74
+// 73 -> i - value
+// 74 ->
+
+// 73 
