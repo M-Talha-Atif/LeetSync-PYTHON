b3e8704754f7db77435257d124cad38a0baeff1a
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-05-12 22:52:24 +0500

    Time: 52 ms (5.14%) | Memory: 44.6 MB (60.15%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+              Map<Integer,Integer> counter = new HashMap<Integer,Integer> ();
+        // counter digits
+        int maxi = Integer.MIN_VALUE;
+        for( int dig : digits){
+            maxi = Math.max(maxi,dig);
+            counter.put(dig, counter.getOrDefault(dig,0) + 1);
+        }
+        List<Integer> res = new ArrayList<Integer>();
+        int end = (maxi+1) * 100;
+        // System.out.println(end);
+
+        for(int i=100; i<end; i++){
+            if ( i % 2 == 0 )
+            {
+                Map<Integer,Integer> map = new HashMap<Integer,Integer> ();
+                int temp = i;
+                int secondTempi = i;
+                while(temp>0){
+                    int digi = temp % 10;
+                    map.put(digi, map.getOrDefault(digi,0) + 1);
+                    temp/=10;
+                }
+
+                boolean check = true;
+                
+                while   (secondTempi>0)   {
+                    int digi = secondTempi % 10;
+                    int a1 = counter.getOrDefault(digi,0);
+                    int a2 = map.getOrDefault(digi,0);
+                    if (a1>3){
+                        a1=3;
+                    }
+                    if (a2 > 3){
+                        a2=3;
+                    }
+
+                    int diff = a1 - a2;
+
+                    if ( diff<0 || diff == a1) {
+                      check = false;
+                      break;
+                    }
+                    secondTempi/=10;
+                }
+
+                // System.out.print("------------");
+                // System.out.println(map);
+                // System.out.println(counter);
+                // System.out.print("------------");
+
+                if (check){
+
+                    res.add(i);
+
+                }
+
+            }
+
+
+        }
+
+        return res.size();
+
+        
+        
+    }
+}
