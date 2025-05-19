0681bb661affea11967bbeaa535af2a71dda572c
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-05-19 11:35:57 +0500

    Time: 1 ms (14.60%) | Memory: 41.7 MB (86.19%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,20 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+
+        // to drop least significant bit we right shift by 1 >>
+        // left shift would add a zero <<
+        if (n == 0) return 0;
+
+        int result = 0;
+
+        for ( int i=0; i<32; i++ ){
+            result <<= 1; // left shift by 1 to add zero
+            if ( ( n & 1 )  == 1) result++;
+            n >>= 1; // right shift by 1 to remove LSB (least significant bit)
+        }
+
+        return result;
+        
+    }
+}
