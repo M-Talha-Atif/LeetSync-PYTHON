58334052658df8b6b103353c92487f5006fdd051
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-05-19 13:43:37 +0500

    Time: 0 ms (100.00%) | Memory: 40.2 MB (79.10%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -3,6 +3,9 @@ class Solution {
 
      int carry;
      // left shift the carry by 1 so that 1 comes at actual position
+     // add the numbers using xor
+     // for carry handling using & and left shift by 1 for carry correct position
+     // accumulate the result in a variable
 
      while (b!=0) {
         carry = ( a & b ); 
