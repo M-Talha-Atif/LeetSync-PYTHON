25aafd147e6a7d214a83501e367dcf33759fe877
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-06-07 20:02:14 +0500

    Time: 39 ms (13.94%) | Memory: 46.3 MB (12.08%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -16,8 +16,11 @@ class Solution {
                data.offer(val);
             }
         }
+        // take out n+1 elements from array with -1 frequency to each
+        // add n+1 to time if que has elements, it will handle idle time as well
+        // add list size if que becomes empty
 
-        
+
         while(!data.isEmpty()){
             List<Integer> temp = new LinkedList<Integer>();
             // n+1 tasks gets solved first
@@ -52,3 +55,36 @@ class Solution {
         
     }
 }
+/* 
+Tasks -> A to Z and a number -> n
+
+n intervals
+AB
+-----
+1 to n+1
+
+3,3
+
+size is now 2, elements are 2,2
+
+2,2
+
+A,B,idle,idle
+-----
+n = 1
+A,B -> n=2, you did two tasks for n=1
+-----
+n = 2
+-----
+A,B,C -> n+1
+
+A,B,C --> one round and n+1 tasks done
+now let say you got only A and y
+
+A had two frequency and n = 2
+characters are A,A,B,C
+
+in one round, 2,1,1 done? so A,B,C done? -> time would be n+1 that is 3 why 3 as we did 3 tasks, if we had A,B only then one idle time would be compensated, 1 is added as 1 task done minumum?
+and n is supposed to be gap or either more tasks.
+Remaining only -> A, frequency is 1 so we got 0 in our list and your que is empty and list having 1, so these are only last tasks no more idle time right?
+Priority que empty so we got, data.size() as time added*/
