1c2b26801ce30fc76f7f56fe85bc3eea9e3a7717
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-06-09 17:22:51 +0500

    Time: 185 ms (61.08%) | Memory: 92 MB (78.53%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
@@ -0,0 +1,47 @@
+class WordDictionary {
+    private WordDictionary[] children;
+    boolean leafNode;
+
+    public WordDictionary() {
+        children = new WordDictionary[26];
+        leafNode = false;
+    }
+    
+    // Adds a word into trie
+    public void addWord(String word) {
+
+        WordDictionary current = this;
+
+        for(char c: word.toCharArray()){
+
+            if(current.children[c - 'a'] == null)
+                current.children[c - 'a'] = new WordDictionary();
+            current = current.children[c - 'a'];
+
+        }
+        current.leafNode = true;
+    }
+    
+    public boolean search(String word) {
+        WordDictionary current = this;
+
+        // dfs for dot part
+
+        for(int i = 0; i < word.length(); ++i){
+            char c = word.charAt(i);
+
+            if(c == '.'){
+                for(WordDictionary ch: current.children)
+                    if(ch != null && ch.search(word.substring(i+1))) return true;
+                return false;
+            }
+
+            // rest same logic
+            if(current.children[c - 'a'] == null) return false;
+            current = current.children[c - 'a'];
+        }
+
+        
+        return current != null && current.leafNode;
+    }
+}
