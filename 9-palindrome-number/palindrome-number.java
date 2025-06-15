d9e9166a2ac6e346efb8a7a555213fb1daf7eb56
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-06-15 21:26:04 +0500

    Time: 5 ms (69.09%) | Memory: 44.6 MB (12.08%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if(x<0) return false;
+
+        var i = 0; // i will hold opposite of x like 12 then here 21
+        var res = x; // it will be zero at the end
+        while(res!=0){
+           i = (i*10) + (res%10);
+           res= res/10;
+        }
+        return x==i;
+        
+    }
+}
