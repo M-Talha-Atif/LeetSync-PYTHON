d8dead9ddeb01b9d6773d8302f0405ad7098907f
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-06-17 16:05:25 +0500

    Time: 0 ms (100.00%) | Memory: 40.9 MB (66.21%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,37 +1,63 @@
 class Solution {
 
-    List<List<Integer>> result;
+/* 
+OR batata hai: "Kaunse bits ka contribution hai."
+
+2^(n-1) batata hai: "Har contributing bit kitni baar aayegi."
+
+Multiply karke hum sab bits ka total sum nikal lete hain.
+
+1,3,5 -> 01, 11, 101
+
+001, 011, 101 -> OR -> 111
+
+2,4 -> 010, 100 -> OR -> 110
+*/
+
+    // List<List<Integer>> result;
+
     public int subsetXORSum(int[] nums) {
 
-        result = new ArrayList<>();
-
-           // take, explore, not take, explore
-        solve(nums, 0, new ArrayList<>());
-
-        int total = 0;
-
-        for(List<Integer> items : result){
-            int temp = 0;
-            for (int item : items){
-                temp ^= item;
-            }
-            total+=temp;
+        int totalSum=0;
+        for(int num : nums){
+            totalSum|= num;
         }
+        // int power = 1 << (n-1);  // 2^(n-1)
+        // 1<<2 is 4, 1<<3 is 8
+        // << this is left shift for power of 2
+        int n = nums.length;
 
-        return total;
+        return totalSum *   ( 1 << (n-1) );
+
+        // result = new ArrayList<>();
+
+        //    // take, explore, not take, explore
+        // solve(nums, 0, new ArrayList<>());
+
+        // int total = 0;
+
+        // for(List<Integer> items : result){
+        //     int temp = 0;
+        //     for (int item : items){
+        //         temp ^= item;
+        //     }
+        //     total+=temp;
+        // }
+
+        // return total;
 
         
     }
 
-    public void solve(int[] nums, int index, List<Integer> temp ){
-        result.add(new ArrayList<>( temp ));
+    // public void solve(int[] nums, int index, List<Integer> temp ){
+    //     result.add(new ArrayList<>( temp ));
 
-        for(int i=index; i<nums.length; i+=1){
-            temp.add(nums[i]); // take
+    //     for(int i=index; i<nums.length; i+=1){
+    //         temp.add(nums[i]); // take
 
-            solve(  nums, i + 1, temp); // process
+    //         solve(  nums, i + 1, temp); // process
 
-            temp.remove( temp.size() - 1 ); // remove again explore
-        }
-    }
+    //         temp.remove( temp.size() - 1 ); // remove again explore
+    //     }
+    // }
 }
