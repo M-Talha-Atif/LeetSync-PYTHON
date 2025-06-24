e27822c7ee00247df0606640aa6b1908ac7ae517
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-06-24 16:44:01 +0500

    Time: 2 ms (97.25%) | Memory: 45.1 MB (38.84%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
@@ -4,18 +4,26 @@ class Solution {
         // FOUND INDEX
         // index 7-> 2,1,3
         // index 5-> 4,6
-       List<Integer> result = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         int n = nums.length;
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (nums[j] == key && Math.abs(i - j) <= k) {
+        int right = 0; 
+    
+        for (int j = 0; j < n; ++j) {
+            if (nums[j] == key) {
+                int left = Math.max(right, j - k);
+                right = Math.min(n - 1, j + k) + 1;
+                for (int i = left; i < right; ++i) {
                     result.add(i);
-                    break; // avoid duplicates
                 }
             }
         }
-            return result;
+        return result;
+        // right always make sure that left startes after from 1 postion after where last right finsihes righ
+        //    2 -> 1,3
+        //    right=3, left=1
+        //    left=3, right=4
+        //    5 -> 4,6
+         
     }
 
 
