41c87c3b6d42118a3c5354a4145f69de07540658
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-07-10 14:47:11 +0500

    Time: 51 ms (5.04%) | Memory: 44 MB (6.88%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,36 +1,37 @@
 class Solution {
     public int mySqrt(int x) {
-        // For special cases when x is 0 or 1, return x.
         if (x == 0 || x == 1)
             return x;
-
+        // search space state is  that is 1 to n
+        // 8 
+        // 1,2,3,4,5,6,7,8
+        // 4 -> mid
+        // 1-4 search space
+        // mid -> 2
+        // 2 * 2 -> 4 < x
+        // 1,2,3,4
+        // 2,3,4 stop now as left and right will be at 2
         int left = 1;
         int right = x;
         int mid = -1;
 
-
         while (left <= right){
-
-             mid =  left + (right - left) / 2;
-
-            // System.out.println("Mid value: " + mid);
+            mid = left + (right - left) / 2;
+            System.out.println(mid);
             int target = mid * mid;
-           
-
-            if (  (long) mid * mid > (long) x ){
-                  right = mid - 1;
+            if ( (long) mid * mid > (long) x){
+                right = mid - 1;
             }
-            else if ( target ==  x){
+            else if ( target == x ){
                 return mid;
+              
             }
             else {
                 left = mid + 1;
             }
 
-
         }
-
-        return right;
         
+        return right;
     }
 }
