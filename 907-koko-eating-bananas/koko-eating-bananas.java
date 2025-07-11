d0e5a228c4b2b25a90925d43e9e846af9c3fa3d7
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-07-11 21:45:49 +0500

    Time: 12 ms (66.52%) | Memory: 45.2 MB (59.67%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,48 +1,58 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        Arrays.sort(piles);
-        int n = piles.length;
-        int left = 1;
-        int right = piles[n-1];
-
-        while ( left < right ) {
-
-            int mid = left + (right - left ) / 2;
-
-            int getAns = giveAnswer(piles, h, mid);
-
-        
-
-            if ( getAns > h){
-                left = mid + 1;
-            }
-    
-            else {
-              
-                right = mid ;
+    // % for remaining bananas
+    // / division for banans eat per hour
+    // 3//3 , 6//3, 7//3 , 11//3
+    // where divisor can be range from 1 to max value of piles
+    public static boolean canEat(int[] piles, int k, int hours)
+    {
+            int countHours = 0;
+            for(int pile : piles){
+                int possibleHours = pile/k;
+                int remaining = pile % k;
+                if ( remaining > 0 ){
+                    countHours+=1;
+                }
+                countHours += possibleHours;
             }
 
-        }
-
-
-        return left;
-
-        
+            return countHours <= hours;
     }
 
-    public int giveAnswer(int[] piles, int h, int k){
-        int total = 0;
-        for(int pile : piles){
-            int div = (pile/k);
-            if ( pile % k != 0){
-                total+=1;
-            }
-            total += div;
-        }
+    public int minEatingSpeed(int[] piles, int h) {
+        // 3,6,7,11
+        // h=8
+        // k -> range
+        // k -> 3 -- 11
+        // pile // k 
+        // 3/3-> 1
+        // 6/3 -> 2
+        // 7/3 -> 3
+        // 11/3 -> 4
+
+        // search space is reduce on basis of true and false
 
         
+        int left= 1;
+        int right=0;
+        for(int pile : piles){
+            right = Math.max(right, pile);
+        }
+        while ( left < right ){
+            int mid = left + (right - left) / 2;
+            boolean result = canEat(piles, mid, h);
+            // false k case m k increase
+            if ( result == false ){
+                left = mid + 1;
+            }
+            else {
+                // in case of equal we look for minimum value
+                right = mid;
+            }
+        }
 
-        return total;
+        return right;
 
+
+        
     }
 }
