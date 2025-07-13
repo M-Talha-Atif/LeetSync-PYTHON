f0eaeb9bff71ce9a89d0e767168a014dc1fbeffb
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-07-13 13:55:08 +0500

    Time: 4 ms (2.11%) | Memory: 45 MB (49.31%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,17 +1,39 @@
 class Solution {
     public int maximumCount(int[] nums) {
-        int cn = 0, zeros = 0;
+        // we can use binary search twice or thrice in case of count
         int length = nums.length;
-        for (int n : nums) {
-            if (n > 0) break;
-            else if (n==0){
-                zeros+=1;
+        // finding positive number using target
+        int left = 0;
+        int right = length - 1;
+
+        int cn = binarySearch( left, right, nums, 0);
+        System.out.println(cn);
+        int cp = binarySearch( left, right, nums, 1);
+        System.out.println(cp);
+
+        // if ( cp==cn && cp==0) return 0;
+       
+        return Math.max( cn, length - cp);
+    }
+    // >=0 for negative
+    // >=1, for positive
+
+
+    public int binarySearch( int left, int right, int[] nums, int comparison){
+         while (left <= right){
+            int mid = left + ( right - left ) / 2;
+            int target = nums[mid];
+            if ( target >= comparison)
+            {
+                right = mid - 1;
             }
-            else if (n<0){
-                cn+=1;
-            }  
-            
+            else {
+                left = mid +1;
+            }
         }
-        return Math.max(cn,  length - cn - zeros);
+        // if ( nums[left]==0 && comparison==1 ){
+        //     return 0;
+        // }
+        return left;
     }
 }
