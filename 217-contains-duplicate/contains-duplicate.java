01b4e4436b20d10a60562fb18efa3f16bdb1b160
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-07-27 22:12:43 +0500

    Time: 13 ms (88.92%) | Memory: 58 MB (84.31%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        // xor operator
+        // 1^1, 0^0 == 0
+        // Space Complexity = Auxiliary Space + Input Space
+        // Time Complexity = running time of a program depending on input size
+        
         HashSet<Integer> data = new HashSet<Integer>();
-        for (Integer i : nums) {
-            if (data.contains(i)){
+
+        for( int n: nums){
+            if ( data.contains(n) ){
                 return true;
             }
-
-            else{
-                data.add(i);
-            }
-                
+            data.add(n);
         }
+
         return false;
         
     }
