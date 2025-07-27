96a073e4de36b2b976c8d0212721b0317a3f704c
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-07-28 01:23:27 +0500

    Time: 3 ms (16.01%) | Memory: 41.5 MB (76.89%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,5 +1,67 @@
 class Solution {
+
+// “Lexicographical sorting is like dictionary sorting — it compares strings character-by-character using their ASCII/Unicode values. If characters are equal, it proceeds to the next. If one string is a prefix of another, the shorter one is considered smaller. For example, with [‘flower’, ‘flow’, ‘flight’], the sorted order is [‘flight’, ‘flow’, ‘flower’] because ‘flight’ has ‘i’ which comes before ‘o’, and ‘flow’ is a prefix of ‘flower
+
+    
+    // Sorting Approach -> n log n + k | k for comparison
     public String longestCommonPrefix(String[] strs) {
+        // minum length so alwasy keep maximum in length
+        // avoid same indexing variable name in nested loops
+        // use charAt for string
+        // use math
+        // edge case -> empty strings array
+        // edge case -> 1 string in whole array
+        // edge case -> break the inner loop if first unmatch comparison in inner loop
+
+        Arrays.sort(strs);
+
+
+      
+        System.out.println(strs[0]);
+        
+        int startLength = 0;
+        String firstString = strs[0];
+        int n = strs.length;
+        if ( n == 1){
+            return  firstString;
+        }
+        // big of n
+        // big of min( len(f), len(k) ) -> big of k
+        // Big of O (n * k)
+
+       
+        String otherString = strs[n-1];
+        int length = Math.min( firstString.length(), otherString.length() );
+
+        for (int i=0; i<length; i+=1 ){
+                if ( otherString.charAt(i) == firstString.charAt(i) )
+                {
+                    startLength+=1;
+
+                } 
+                else {
+                    break;
+                }
+        }
+        System.out.println(startLength );
+
+        if ( startLength == 201){
+            startLength -= 201;
+        }
+
+        return firstString.substring( 0, startLength);
+        
+        
+    }
+
+
+}
+
+    // ----------------------------
+
+
+    // Brute Force Approach
+    // public String longestCommonPrefix(String[] strs) {
         // minum length so alwasy keep maximum in length
         // avoid same indexing variable name in nested loops
         // use charAt for string
@@ -12,41 +74,43 @@ class Solution {
         // sab se smallest ko compare krlu longest se
         // flow, flower
         // smallest flo, flr
-        // horizontal matching, pick up first string compare it with rest
-        int startLength = 201;
-        String firstString = strs[0];
-        int n = strs.length;
-        if ( n == 1){
-            return  firstString;
-        }
-        // big of n
-        // big of min( len(f), len(k) ) -> big of k
-        // Big of O (n * k)
+    //     // horizontal matching, pick up first string compare it with rest
+    //     Arrays.sort(strs);
+    //     System.out.println(strs[0]);
+    //     int startLength = 201;
+    //     String firstString = strs[0];
+    //     int n = strs.length;
+    //     if ( n == 1){
+    //         return  firstString;
+    //     }
+    //     // big of n
+    //     // big of min( len(f), len(k) ) -> big of k
+    //     // Big of O (n * k)
 
-        for (int index=1; index<n; index++){
-            String otherString = strs[index];
-            int length = Math.min( firstString.length(), otherString.length() );
-            int count = 0;
-            for (int i=0; i<length; i+=1 ){
-                if ( otherString.charAt(i) == firstString.charAt(i) )
-                {
-                    count+=1;
+    //     for (int index=1; index<n; index++){
+    //         String otherString = strs[index];
+    //         int length = Math.min( firstString.length(), otherString.length() );
+    //         int count = 0;
+    //         for (int i=0; i<length; i+=1 ){
+    //             if ( otherString.charAt(i) == firstString.charAt(i) )
+    //             {
+    //                 count+=1;
 
-                } 
-                else {
-                    break;
-                }
-            }
-            startLength = Math.min(startLength,count);
-        }
-        System.out.println(startLength );
+    //             } 
+    //             else {
+    //                 break;
+    //             }
+    //         }
+    //         startLength = Math.min(startLength,count);
+    //     }
+    //     System.out.println(startLength );
 
-        if ( startLength == 201){
-            startLength -= 201;
-        }
+    //     if ( startLength == 201){
+    //         startLength -= 201;
+    //     }
 
-        return firstString.substring( 0, startLength);
+    //     return firstString.substring( 0, startLength);
         
         
-    }
-}
+    // }
+
