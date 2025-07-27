f784f2d925f4fb7f599590312b129d5494feab95
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-07-27 22:05:20 +0500

    Time: 2 ms (7.84%) | Memory: 45.6 MB (30.47%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -1,19 +1,18 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
+        // 1,2,1 -- 1,2,1
+        // 2 times placement
+        // n * 2 -> size 
         int n = nums.length;
-        int[] result = new int[ 2 * n ];
-        // fill twice at the time
-        for(int i =0; i<n; i+=1){
+        // intially array all elements are zero
+        int[] result = new int[n*2];
+        System.out.println( result[0] );
+        for(int i=0; i<n; i++){
             result[i] = nums[i];
-            result[i+n] = result[i];
-            // nums -> [1,2,1]
-            // assign like
-            // 0 -> 1, 0 + 3 -> 3 -> 1 (value)
-            // 1 -> 2, 1 + 3 -> 4 -> 2 (value)
-
+            result[i+n] = result[i]; 
         }
-        return result;
 
+        return result;
         
     }
 }
