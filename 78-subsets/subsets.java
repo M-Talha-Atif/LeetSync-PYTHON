b4dd1df025e8a93795ab0eaaeb14e17e08ecbe86
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-07-28 16:41:15 +0500

    Time: 1 ms (49.69%) | Memory: 42.8 MB (65.44%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,25 +1,26 @@
 class Solution {
-    List<List<Integer>> result;
+    /* 
+Recursive calls:
+Each call will get its copy of temporary list and also of start
+1 -> 1,2, -> 1,2,3 | start =0, start = 1, start = 2
+1 -> 1,3, -> 1,3 |  start =0, start = 2
+2 ->  2,3 | start =1, start = 2
+
+    */
     public List<List<Integer>> subsets(int[] nums) {
-
-
-        result = new ArrayList<>();
-        // take, explore, not take, explore
-        solve(nums, 0, new ArrayList<>());
-
-        return result;
-        
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
     }
 
-    public void solve(int[] nums, int index, List<Integer> temp ){
-        result.add(new ArrayList<>( temp ));
-
-        for(int i=index; i<nums.length; i+=1){
-            temp.add(nums[i]); // take
-
-            solve(  nums, i + 1, temp); // process
-
-            temp.remove( temp.size() - 1 ); // remove again explore
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
+        list.add(new ArrayList<>(tempList));
+        for (int i = start; i < nums.length; i++) {
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
         }
     }
+
 }
