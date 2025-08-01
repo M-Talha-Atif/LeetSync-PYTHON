dce38a6ea904ed4a6e09f70e8b4e5ed8c61627a6
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-08-02 00:03:52 +0500

    Time: 11 ms (99.98%) | Memory: 47 MB (98.44%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -1,27 +1,60 @@
 class MyHashSet {
+    // 00000000 00000000 00000000 00000010
+    // above are 32 bits for 1 integer
+    // 0-31 would be treated as number and specific bit will be turned on/off
+    // after adding 1,2  --->  num[0] = 00000000 00000000 00000000 00000110
+    // after adding 3,4,5 ---> num[0] = 00000000 00000000 00000000 00111110
+
+    // unset scenario
+    /* 
+num[0] = 00000000 00011100 00000000 00111110
+mask(3) = 00000000 00000000 00000000 00001000
+~mask   = 11111111 11111111 11111111 11110111
+AND:
+00000000 00011100 00000000 00111110
+& 11111111 11111111 11111111 11110111
+-------------------------------------
+  00000000 00011100 00000000 00110110
+    
+    */
     int num[];
+
+
     public MyHashSet() {
+
         num = new int[31251];
+        
     }
-	
-	// set the bit if that element is present
+    
+    // set the bit if that element is present
+    // using the or operator
+    // | -> or operator
     public void add(int key) {
-        num[getIdx(key)]|=getMask(key);
+        num[getIdx(key)] |= getMask(key);
     }
-	
-	//unset the bit if a key is not present
+    
     public void remove(int key) {
-        num[getIdx(key)] &= (~getMask(key));
+        num[getIdx(key)] &= (   ~getMask( key)  );
+
+       
+        
     }
-	
-	//check if bit corresponding to the key is set or not
+    
     public boolean contains(int key) {
-        return (num[getIdx(key)]&getMask(key))!=0;
+        // if mask are different it means key is there
+        // it will compare the bit only
+        // 1 0 1 let say in num[0]
+        // key to check is 2
+        // so mask of 2 is 1  0  0
+        // 1 0 1 & 1 0 0 is 1 0 0 returned value is greater than 1, return true
+        // if key is 0
+        // then 1 0 1 & 0 0 1 is  0 0 1, return true
+
+        return  ( num[ getIdx(key)] & getMask(key) ) !=0; 
+        
     }
-	
-	// idx of num[] to which this key belongs
-	// for key = 37, it will give 1
-    private int getIdx(int key)
+
+     private int getIdx(int key)
     {
         return (key/32);
     }
@@ -33,33 +66,11 @@ class MyHashSet {
         return (1<<key);
     }
 }
-/* 
 
-
-Each array item holding a group of numbers.
-
-nums[0] -> 0-31
-nums[1] -> 32-63
-
-For 37, we divided it by 32 to find its group so we got 1 that is index as 1
-
-nums[1] -> 37 would be here, but how to distinguish?
-
-For distinguishing like for nums[1] where 37 is present we will use a bit for tracking 
-as set or not. As I mentioned, nums[1] will have numbers from 32-63 that are total of 32 numbers.
-So, each int have 32 bits. Each number will get a bit as set or not. 
-
-Set -> 1, Unset -> 0
-
-For Set,Unset, we take module with 32 to get position of a number.
-
-37%32 -> 5
-
-So, we set the 5th bit in whole of 32 bits to be set.
-
-For 36, it would be 4
-For 35, it would be 3
-
-In this way, using a single bit in each group we stored numbers, removed and checked either they exists.
-
-*/
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
