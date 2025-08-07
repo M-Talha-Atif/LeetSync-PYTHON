c378fcb827b5b72db7375edc0e15d5d7106888c0
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-08-07 23:32:24 +0500

    Time: 2 ms (87.29%) | Memory: 55.5 MB (50.02%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -3,21 +3,29 @@ class Solution {
         // for each element pre calculate prefix product and suffix product
         // like for 2 it will be 1 for prefix and suffix will be 12
         int n = nums.length;
-        int[] pref = new int[n];
-        int[] suffix = new int[n];
-        pref[ 0 ] = 1;  // answer will be same for first element and last one
-        suffix[ n-1 ] = 1;
-        for (int i=1; i < n; i++){
-            pref[i] = pref[i-1] * nums[i-1];
+        // int[] pref = new int[n];
+        // int[] suffix = new int[n];
+        // pref[ 0 ] = 1;  // answer will be same for first element and last one
+        // suffix[ n-1 ] = 1;
+        int[] ans = new int[n];
+        int mult = 1;
+        // each time we have previous value
+        for (int i=0; i < n; i++){
+            ans[i] = mult;
+            mult *= nums[i];
+           
         }
-        for (int i=n-2; i >=0; i--){
-            suffix[i] =  suffix[i+1] * nums[i+1];
+        mult = 1;
+        for (int j=n-1; j >=0; j--){
+            // can't use = as previous pref value will be gone
+            ans[j] *= mult;
+            mult *= nums[j];
         }
-         for(int i = 0; i < n; i++) {
-            nums[i] = pref[i] * suffix[i];
-        }
+        
+        // pref -> 1, 1, 2, 6
+        // suff ->  24 12 4  1
 
-        return nums;
+        return ans;
         
     }
 }
