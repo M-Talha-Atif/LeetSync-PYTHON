7232fa1705c5f9135fb53b508bd08e135a767cbe
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-08-10 23:27:30 +0500

    Time: 3 ms (20.02%) | Memory: 45 MB (9.34%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -14,23 +14,33 @@
  * }
  */
 class Solution {
-     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p==null && q==null) return true;
-        if ( (p==null && q!=null) || (q==null && p!=null) || (p.val!=q.val) ) return false;
 
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // edge cases
+        if ( p == null && q==null){
+            return true;
+        }
+        if ( p == null && q!=null  ){
+            return false;
+        }
+        if ( p != null && q==null  ){
+            return false;
+        }
+        if ( p.val != q.val ){
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         
     }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(root==null)
           return false;
-     
-        if (isSameTree(root,subRoot)){
-            return true;
-        }
+        boolean r1 = isSameTree( root, subRoot);
+        boolean r2 = isSubtree( root.left, subRoot);
+        boolean r3 = isSubtree( root.right, subRoot);
 
-
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        return r1 || r2 || r3;
         
     }
 }
