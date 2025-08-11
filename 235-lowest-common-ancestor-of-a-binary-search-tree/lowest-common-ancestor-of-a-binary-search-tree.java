a3f2e202b0de8eabe3595239cf9b2c30b58e7db6
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-08-11 20:16:41 +0500

    Time: 5 ms (100.00%) | Memory: 44.9 MB (80.63%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -13,27 +13,34 @@ class Solution {
     // node value if greater than both p,q then recurs to left
     // node value if less than both p,q then recurs to right
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null){
-            return root;
-        }
-        // conditions on matching
-        if ( root.val > p.val && root.val > q.val){
-            // look for smaller
-            TreeNode left = lowestCommonAncestor( root.left, p, q);
-            if  (left!=null ){
-                return left;
+        // if (root == null){
+        //     return root;
+        // }
+        // // conditions on matching
+        // if ( root.val > p.val && root.val > q.val){
+        //     // look for smaller
+        //     TreeNode left = lowestCommonAncestor( root.left, p, q);
+        //     if  (left!=null ){
+        //         return left;
+        //     }
+        // }
+        // else if ( root.val < p.val && root.val < q.val){
+        //     // look for greater
+        //     TreeNode right = lowestCommonAncestor( root.right, p, q);
+        //     if  ( right !=null )
+        //     {
+        //         return  right;
+        //     }
+        // }
+        // iterative explore each node
+        while (root != null) {
+        if (p.val < root.val && q.val < root.val) root = root.left;
+        else if (p.val > root.val && q.val > root.val) root = root.right;
+        else return root; // Split found
             }
-        }
-        else if ( root.val < p.val && root.val < q.val){
-            // look for greater
-            TreeNode right = lowestCommonAncestor( root.right, p, q);
-            if  ( right !=null )
-            {
-                return  right;
-            }
-        }
+    return null;
 
-        return root;
+   
         
     }
 }
