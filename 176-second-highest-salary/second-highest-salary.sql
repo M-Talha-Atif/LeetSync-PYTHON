baf23524b19c0c798f3ce2e6f2881cc1bcddbba3
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-08-25 18:03:15 +0500

    Time: 315 ms (18.14%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,13 +1,4 @@
 # Write your MySQL query statement below
+--  two times max function
 select max(salary) as SecondHighestSalary from employee where 
-  employee.salary < ( select max(salary)  from employee );
-
--- The difference lies in how SQL handles empty result sets. In the single SELECT, if no row is found, nothing is returned.
--- In the nested SELECT, when the subquery returns no result, it explicitly returns NULL, which the outer query then displays.
-
-
--- select
--- (select distinct Salary 
--- from Employee order by salary desc 
--- limit 1 offset 1) 
--- as SecondHighestSalary;
+salary < ( select max(salary) from employee );
