900bcb38aa8df4c8077c0738621c6c8f1e189432
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-08-25 18:05:48 +0500

    Time: 447 ms (47.74%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -3,19 +3,8 @@ BEGIN
   DECLARE m INT;
   SET m = N - 1;
   RETURN (
-    -- limit -> how many no of rows i will return
-    --  does there duplicates? is n greater than enteries
       # Write your MySQL query statement below.
-    --   300 200 100, n log n
-    --  offset skips the rows
-    --  limit return the rows
-    --  row number starts from 0
-    select distinct salary from Employee order by salary desc
-    limit m, 1
-    --  limit n-1 -> skip the rows, next value to return
-    --  limit 1 offset n-1, offset for skipping
-    --  m -> no of rows to skip
-    --  1 -> return the rows
+      select distinct salary from employee order by salary desc limit m, 1
 
   );
 END
