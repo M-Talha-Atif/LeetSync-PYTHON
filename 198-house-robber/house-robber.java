15e6504236ea5e3232d781fb90282d592357a8fd
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-09-11 15:59:08 +0500

    Time: 0 ms (100.00%) | Memory: 41.3 MB (25.00%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,9 +1,28 @@
 class Solution {
+    // two states
+    // skip current house then prev houses money stored dp[i-1]
+    // take current house then pick money from house 2 steps behind dp[i-2]
     public int rob(int[] nums) {
-        Map<Integer,Integer> memo = new HashMap<Integer, Integer>();
 
-        return rob( nums, memo, 0 );
+        return rob( nums, 0, nums.length );
     }
+    private int rob( int[] nums, int start, int end){
+        int prev1=0; // current money
+        int prev2=0; // money before prev1
+        
+        for (int num : nums) {
+            int robCurrent = num + prev2; // Rob this house
+            int skipCurrent = prev1;      // Skip this house
+
+            int curr = Math.max(robCurrent, skipCurrent);
+
+            prev2 = prev1; // Slide window
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
     private int rob(int[] nums, Map<Integer,Integer> memo, int index){
 
         if ( index >= nums.length ){
