f375e1f7c353482c36eee475f5627b0be17a87ae
Muhammad Talha Atif <108191213+Progambler227788@users.noreply.github.com>
2025-09-17 15:16:47 +0500

    Time: 17 ms (21.59%) | Memory: 55.4 MB (11.83%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -1,38 +1,30 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        // limit = 3
 
-        // 1,2,2,3
-        // 1,2, count+1
-        // 3,3,4,5
-        // 1,2,2,3 target = 7
-        // 1,2,2,
-
-        // current 1 boat
-        // 
+        // sorting + moving pointers
+        // how to do pairing
         Arrays.sort(people);
-        // sit big people and see if space then pick small
-        // if small can't fit, allocate seat 1 for right
+        int left = 0;
+        int right = people.length - 1;
         int boats = 0;
-        int left =0, right = people.length-1;
-     
-        while(left <=right){
-
-            int accomodate = people[left] + people[right];
-
-            if (accomodate<=limit){
-                boats+=1; // both small and big done
-                left+=1; right-=1;
-            }
-            // can't fit small people so give seat to big one
-            else {
-                boats+=1; // big done
+        while (left <= right){
+            int remaining = limit - people[right];
+            if ( remaining  ==  0){
                 right-=1;
-
             }
-          
-            
+            else if ( remaining  >  0 && remaining - people[left] >=0 ){
+                right-=1;
+                left+=1;
+            }
+            else {
+                right-=1;
+            }
+            boats+=1; 
         }
+        // 1,2,2,3
+        // 3, right is now at 2
+        // 1,2,2
+        // 
 
         return boats;
         
