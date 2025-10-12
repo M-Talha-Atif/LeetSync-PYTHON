b4b224a6423ece9f4f630e71d50d3ba27768ceec
Muhammad Talha Atif <108191213+M-Talha-Atif@users.noreply.github.com>
2025-10-12 13:51:32 +0500

    Time: 0 ms (100.00%) | Memory: 40.3 MB (82.62%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,27 +1,21 @@
 class Solution {
     public int tribonacci(int n) {
-        // 3 base cases numbers that is 0, 1 , 1
-        // can be solved using for loop, without map
-
         if (n == 0)
             return 0;
         if (n == 1 || n == 2)
             return 1;
-
-        int a = 0, b = 1, c = 1;
-        for (int i = 3; i <= n; i++) {
-            int next = a + b + c;
-            // resule change now
-            // 0, 1 , 1 = 2
-            // 1, 1, 2 = 4
-            // just excchange variables
+        int a = 0;
+        int b = 1;
+        int c=  1;
+        int current = 0;
+        for ( int i=3; i<=n; i++){
+            current = a + b + c;
             a = b;
             b = c;
-            c = next;
+            c = current;
         }
+
         return c;
-
+        
     }
-
-   
 }
