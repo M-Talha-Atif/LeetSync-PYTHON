0bee68b7834126af27b16dff6fe801ca9fba8cbd
Muhammad Talha Atif <108191213+M-Talha-Atif@users.noreply.github.com>
2025-10-12 13:26:57 +0500

    Time: 0 ms (100.00%) | Memory: 40.4 MB (67.59%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,24 +1,29 @@
 class Solution {
     public int climbStairs(int n) {
-
-        Map<Integer, Integer> memo = new HashMap<>();
-
-
-        return climbStairs(n,memo);
-        
-    }
-
-    private int climbStairs(int n, Map<Integer, Integer> memo ) {
-
-        if ( n==0 || n==1){
+        // you can take one step 
+        // you can take two steps
+        // n
+        if (n==0 || n==1){
             return 1;
         }
-        if ( !memo.containsKey(n)){
-            memo.put( n, climbStairs(n-1, memo) + climbStairs(n-2, memo) );
+        int prev=1;
+        int prev1=1;
+        int total = 0;
+        for ( int i=1; i<n; i++){
+            total = prev + prev1;
+            prev = prev1;
+            prev1 = total;
         }
+        return total;
 
-        return memo.get(n); // already present case
-        
     }
+// 0 -> 1
 
+// 1 -> 1
+
+// 2 -> 2 | depends on 0 and 1
+
+// 3 -> 3 | depends on 2 and 1
+
+// 4 -> 5 | depends on 3 and 2
 }
