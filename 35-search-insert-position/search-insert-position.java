155f60424dafd156685a8575195a1f70ac4fc5bc
Muhammad Talha Atif <108191213+M-Talha-Atif@users.noreply.github.com>
2025-10-28 23:06:00 +0500

    Time: 0 ms (100.00%) | Memory: 42.8 MB (93.45%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,26 +1,23 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int left = 0;
-        int right = nums.length-1;
-        //
-        while ( left < right ){
+        int right = nums.length - 1;
+        while (left <= right ){
             int mid = left + (right - left) / 2;
-
-            if ( target == nums[mid] ){
+            int x = nums[mid];
+            if (x == target){
                 return mid;
             }
-            else if (target < nums[mid]){
-                right = mid;
+            else if (x > target) {
+                right = mid - 1;
             }
             else {
                 left = mid + 1;
             }
-        }
-        // <=, in case insert at start or at same index
-        // > next to it
-        // bound will stop at left index
 
-        return nums[left]<target ? left+1 : left;
+        }
+
+        return left;
         
     }
 }
