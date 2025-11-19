896022cb4fbf79f4dda9c4cebc4d5592e5f57b80
Muhammad Talha Atif <108191213+M-Talha-Atif@users.noreply.github.com>
2025-11-19 16:21:05 +0500

    Time: 0 ms (100.00%) | Memory: 43.4 MB (23.73%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
@@ -1,28 +1,36 @@
 class Solution {
-    public boolean isAlienSorted(String[] W, String O) {
-        // store index of characters as values with keys as characters
-        // fetch each character of word 1 and word 2 compare them
-        // do it for all pair of pair of words
-        Map<Character,Integer> order = new HashMap<>();
-        for (int i = 0; i < O.length(); i++)
-            order.put(O.charAt(i), i);
-        for (int i = 1; i < W.length; i++) {
-            String a = W[i-1];
-            String b = W[i];
-            for (int j = 0; j < a.length(); j++) {
-                if (j == b.length()){
-                    return false;
-                }
-                char cha = a.charAt(j);
-                char chb = b.charAt(j);
-                if (order.get(cha) < order.get(chb)){
-                    break;
-                }
-                if (order.get(cha) > order.get(chb)){
-                    return false;
-                }
+    public boolean isAlienSorted(String[] words, String order) {
+        // order rank
+        // sorted check we do pair matching so thing is we need to check only two words at a time
+        int[] rank = new int[26];
+        for (int i = 0; i < order.length(); i++) {
+            rank[order.charAt(i) - 'a'] = i;
+        }
+        for ( int j=1; j < words.length;  j++){
+             if ( ! isSorted( words[j-1], words[j], rank) ){
+                  return false;
+             }
+        }
+
+        return true;
+        // words matching logic
+        // two chracters are equal, then check their rank  
+    }
+    public boolean isSorted( String w1, String w2, int[] rank)
+    { 
+        int length = Math.min( w1.length(), w2.length() );
+        for ( int i=0; i<length; i++){
+            char a = w1.charAt(i);
+            char b = w2.charAt(i);
+            if ( a != b){
+                int rank1 = rank[ a - 'a'];
+                int rank2 = rank[ b - 'a'];
+                return rank1 < rank2;
             }
         }
-        return true;
+
+        return w1.length() <= w2.length();
+
+
     }
 }
