15e38d10d6c9df8e222dbffd3af961a8faefebc5
Muhammad Talha Atif <108191213+M-Talha-Atif@users.noreply.github.com>
2025-12-27 18:19:28 +0500

    Time: 0 ms (100.00%) | Memory: 43.8 MB (11.62%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
@@ -1,30 +1,41 @@
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
-        // two word pairs comparison on the basis of order
-        int[] rank = new int[26];
-        for ( int i=0; i<order.length(); i++){
-            char c = order.charAt(i);
-            rank[ c -'a' ] = i; // assign the order from index
-        }  
-         for ( int i=1; i < words.length; i++){
-            if (! isSorted( words[i-1], words[i], rank )){
+        int n = order.length();
+        int rank[] = new int[n];
+        for (int index=0; index<n; index++){
+            char c  = order.charAt(index);
+            int asciiValue = c - 'a';
+            rank[ asciiValue ] = index; // assign the index for tracking the rank 
+        }
+        for (int i=1; i<words.length; i++){
+            String word1 = words[i-1];
+            String word2 = words[i];
+            if ( !areWordSorted( word1, word2, rank ) ){
                 return false;
-            }
-        }  
-        return true; 
-        
-    }
-    public boolean isSorted(String word1, String word2, int[] rank){
-        int minLength = Math.min(word1.length(), word2.length()); // minimum length for comparison
-        for (int i=0; i<minLength; i++){
-            char c1 = word1.charAt(i),c2 = word2.charAt(i);
-            if ( c1 != c2 ){
-                // first unmatched character rank to determine sorted or not
-                return rank[c1-'a'] < rank[c2-'a'];
             }
         }
 
-        return word1.length() <= word2.length();
-
+        return true;   
+        
     }
+
+    private boolean areWordSorted( String word1, String word2 , int[] rank){
+       
+
+        int minLength = Math.min( word1.length(), word2.length() );
+
+        for ( int i=0; i < minLength; i++){
+            char c1 = word1.charAt( i );
+            char c2 = word2.charAt( i );
+            if (c1 != c2 ){
+                // first mis-matching rank
+                return rank[c1 -'a'] < rank[c2 -'a'];
+            }
+        }
+        if ( word1.length() > word2.length()) return false;
+
+        return true;
+
+
+    } 
 }
