2d8838c7595efd4559b9f8fb2c13fbf558e5bbce
Muhammad Talha Atif <108191213+M-Talha-Atif@users.noreply.github.com>
2026-01-10 23:25:51 +0500

    Time: 0 ms (100.00%) | Memory: 43.6 MB (25.33%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0; i<nums.length;i++){
             if ( nums[i] != val )
             {
-                  nums[index++] = nums[i];
+                nums[index++] = nums[i];
             }
         }
 
