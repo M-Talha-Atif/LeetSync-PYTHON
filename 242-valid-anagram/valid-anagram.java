c1ca1131889bd96d07ee7b40e844192ea730ece9
Muhammad Talha Atif <108191213+M-Talha-Atif@users.noreply.github.com>
2026-01-10 22:45:11 +0500

    Time: 13 ms (28.70%) | Memory: 46.8 MB (6.08%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,21 +1,30 @@
 class Solution {
-
-      // To sort a string alphabetically
-    public static String sortString(String inputString)
-    {
-        // Converting input string to character array
-        char tempArray[] = inputString.toCharArray();
-
-        // Sorting temp array using
-        Arrays.sort(tempArray);
-
-        // Returning new sorted string
-        return new String(tempArray);
-    }
-
     public boolean isAnagram(String s, String t) {
 
-        return sortString(s).equals(sortString(t));
+        // anagram, nagaram
+        // math
+        // 1 map for incrementing frequency
+        // 1 map for decrementing frequency
+        // space complexity big o of n
+        // time complexity big of n
+
+         HashMap<Character,Integer> counter = new HashMap<>();
+
+        // count frequencies for  s string increment
+        for( char character : s.toCharArray() ) {
+            counter.put(character, counter.getOrDefault(character,0) + 1);
+        }
+        // decrement the same characters of both strings
+        for( char character : t.toCharArray() ) {
+            counter.put(character, counter.getOrDefault(character,0) - 1);
+        }
+        
+        // result should be zero if both count same
+        for( int freq : counter.values() ){
+            if (freq!=0) {return false;}
+        }
+
+        return true;
         
     }
 }
