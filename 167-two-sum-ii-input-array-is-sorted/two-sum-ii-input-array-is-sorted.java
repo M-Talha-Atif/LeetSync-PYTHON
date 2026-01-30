8573e85ce4105295aa2c9352e5e1024d72bebb28
Muhammad Talha Atif <108191213+M-Talha-Atif@users.noreply.github.com>
2026-01-30 22:15:21 +0500

    Time: 2 ms (96.27%) | Memory: 48.6 MB (19.23%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,22 +1,29 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        HashMap<Integer,Integer> counter =  new HashMap<Integer,Integer>();
-        int[] arr = new int[2];
 
-        for(int index=0; index<numbers.length; index++){
-            int num = numbers[index];
-            int diff = target - num;
-            int retrievedIndex = counter.getOrDefault(diff,-1);
-            if( retrievedIndex!=index &&  retrievedIndex  != -1){
-                arr[0] = retrievedIndex+1;
-                arr[1] = index+1;
-                return arr;
+        // two pointers
+        // left - right 
+        // 2, 15 = 17 , right minus
+        // 2 , 11 = 13, right minus
+        // 2,7 = 9 ok ay, right minus
+        // big O(n) - in case on pointer is getting decreased
+        int left = 0;
+        int right = numbers.length - 1;
+        // same number can't be used so <
+        while ( left < right ) {
+            int total = numbers[left] + numbers[right];
+            if ( total > target ) {
+                right -= 1;
             }
-
-            counter.put(num, counter.getOrDefault(num,index));
+            else if ( total == target ){
+                return new int[] {left+1, right+1};
+            }
+            else {
+                left+=1;
+            }
         }
 
-        return arr;
+        return new int[] {left+1, right+1};
         
     }
 }
