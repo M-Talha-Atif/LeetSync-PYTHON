75bf523bd96b8458b72483a4e06b0a642f94494f
Muhammad Talha Atif <108191213+M-Talha-Atif@users.noreply.github.com>
2026-03-31 23:01:24 +0500

    Time: 30 ms (72.26%) | Memory: 146.6 MB (67.09%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,35 +1,37 @@
+import java.util.*;
+
 class Solution {
+
     public int[] maxSlidingWindow(int[] nums, int k) {
-    int n = nums.length;
-    int[] result = new int[n - k + 1];
-    TreeMap<Integer, Integer> map = new TreeMap<>();
 
-    for (int i = 0; i < n; i++) {
-        
-        // Add current element
-        map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        Deque<Integer> dq = new ArrayDeque<>();
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
 
-        // Remove element going out of window
-        if (i >= k) {
-            int out = nums[i - k];
-            if (map.get(out) == 1) map.remove(out);
-            else map.put(out, map.get(out) - 1);
+        int left = 0;
+
+        for(int right = 0; right < n; right++){
+
+            // 1. Remove smaller elements
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[right]){
+                dq.pollLast();
+            }
+
+            dq.addLast(right);
+
+            // 2. Remove out-of-window
+            if(dq.peekFirst() < left){
+                dq.pollFirst();
+            }
+
+            // 3. Window of size k reached
+            if(right - left + 1 == k){
+
+                result[left] = nums[dq.peekFirst()];
+                left++;
+            }
         }
 
-        // Add max to result
-        if (i >= k - 1) {
-            result[i - k + 1] = map.lastKey(); // max in current window
-        }
-    }
-
-    return result;
-
-
-
-
-
-
-
-        
+        return result;
     }
 }
